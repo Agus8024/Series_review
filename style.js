e9b21c858c925dd39d9c4993849e33c832ebdd51
Agus8024 <agusrosato14@gmail.com>
2024-05-14 23:03:26 -0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Aquí puedes agregar la lógica para enviar el formulario, por ejemplo, a través de AJAX
+    alert("Formulario enviado. ¡Gracias por tu consulta!");
+});
